bc34c1f0be303030f36910b40af7857d1a10ac6d
Ning Kang <ningkang@gmail.com>
2015-06-05 15:47:25 -0500

    ,
    ex7

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
+#compiler to use
+CC = cc
+#compiler options
 CFLGAS=-Wall -g
+LDFLAGS=
+SOURCES= ex7.c
+OBJECTS=$(SOURCES)
+EXECUTABLE=hello
+
+all: $(SOURCES) $(EXECUTABLE)
+
+$(EXECUTABLE):
+	$(CC) $(CFLAGS) $(OBJECTS) -o $@
+
+#.c:
+#	$(CC) $(CFLAGS) $< -o $@
 
 clean:
-	rm -f ex1
+	rm -f $(EXECUTABLE)
--- ex1.c
+++ ex1.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    puts("Hello World");
+    int distance = 100;
+    float power = 26.50f;
+    double super_power = 56789.4532;
+    char initial = 'A';
+    char first_name[] = "Zed";
+    char last_name[] = "Shaw";
+
+    char fArray[4];
+
+    memcpy(fArray, &power, sizeof(float)); 
+
+    printf("You are %d miles away.\n", distance);
+    printf("You have %f levels of power.\n", power);
+    printf("You have %f awesome super power\n", super_power);
+    printf("I have an initial %c\n", initial);
+    printf("I have a first name %s\n", first_name);
+    printf("I have a last name %s\n", last_name);
+    printf("My whole name is %s %c. %s\n", first_name, initial, last_name);
+
+    printf("0x%02x, 0x%02x, 0x%02x, 0x%02x\n", fArray[0], fArray[1], fArray[2], fArray[3]);
 
     return 0;
 }
